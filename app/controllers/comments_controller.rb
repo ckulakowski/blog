82b5b6e7ae98351c64ccf5c59707aed5cb171e08
Caitlyn Kulakowski <ckulakowski@gmail.com>
2014-02-14 10:31:40 -0500

    http authentication
    etc

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,19 @@
 class CommentsController < ApplicationController
   def new
     @post = Post.find(params[:post_id])
-    binding.pry
     @comment = @post.comments.new
   end
   
+  http_basic_authenticate_with name: "katy", password: "nicholas", only: :destroy
   def create
-    raise "satan"
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment])
+    redirect_to post_path(@post)
+  end
+  
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    redirect_to post_path(@post)
   end
 end
